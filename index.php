bce815504682a237d64c42de893f171c46a9e7cb
Naohide Takahashi <cattyneo@gmail.com>
2020-10-25 18:26:03 +0900

    fix html & add works data (php-array)

--- index.php
+++ index.php
@@ -1,5 +1,58 @@
 <?php
 
+// [id => work]
+// skills: 使用技術
+// roles: 担当箇所
+$works = [
+    1 => [
+        'title' => '株式会社T様 コーポレートサイト',
+        'date' => '2020.08',
+        'url' => '',
+        'skills' => [],
+        'roles' => [],
+        'description' => '',
+    ],
+    2 => [
+        'title' => '株式会社O様 コーポレートサイト',
+        'date' => '2020.03',
+        'url' => '',
+        'skills' => [],
+        'roles' => [],
+        'description' => '',
+    ],
+    3 => [
+        'title' => '株式会社M様 コーポレートサイト(デザイン)',
+        'date' => '2020.02',
+        'url' => '',
+        'skills' => [],
+        'roles' => [],
+        'description' => '',
+    ],
+    4 => [
+        'title' => '株式会社R様 コーポレートサイト',
+        'date' => '2017.09',
+        'url' => '',
+        'skills' => [],
+        'roles' => [],
+        'description' => '',
+    ],
+    5 => [
+        'title' => '株式会社J様 コーポレートサイト',
+        'date' => '2017.06',
+        'url' => '',
+        'skills' => [],
+        'roles' => [],
+        'description' => '',
+    ],
+    6 => [
+        'title' => '株式会社M様 コーポレートサイト',
+        'date' => '2017.04',
+        'url' => '',
+        'skills' => [],
+        'roles' => [],
+        'description' => '',
+    ],
+];
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -14,19 +67,19 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <header>
-        <a href="#contact" id="header-btn-contact"><i aria-label="お問い合わせ" class="far fa-envelope"></i></a>
+    <header class="header">
+        <div class="container d-flex">
+            <a href="#contact" id="header-btn-contact" class="header-btn"><i aria-label="お問い合わせ" class="far fa-envelope"></i></a>
+        </div>
     </header>
 
-    <section id="intro" class="section">
+    <section id="intro" class="section section-intro">
         <div class="container text-center">
             <h1 class="h1">
                 <div class="subtitle">Web Developer</div>
                 <div>Naohide Takahashi</div>
             </h1>
-            <div>
-                <img src="./images/img_profile.jpg" alt="プロフィール写真" width="140" height="140">
-            </div>
+            <img class="photo-intro" src="./images/img_profile.jpg" alt="プロフィール写真" width="160" height="160">
             <p>現役Webエンジニア・デザイナー・ディレクター。<br>
                 埼玉県在住、32歳、一児の父。<br>
                 “広くて深い何でも屋”を目指しています。
@@ -34,116 +87,145 @@
         </div>
     </section>
 
-    <section id="skills" class="section">
+    <section id="skills" class="section section-skills">
         <div class="container text-center">
             <h2 class="h2">
                 <div>Skills</div>
-                <div class="subtitle">実務制作経験のある言語・ライブラリ等を紹介します。</div>
+                <div class="subtitle">実務経験のある言語・ライブラリ・技術を紹介します。</div>
             </h2>
 
             <ul class="box-group box-group-skills">
                 <li class="box-group-item">
                     <i aria-hidden="true" class="skill-icon fab fa-js-square"></i>
-                    <h3 class="h3">Front-end</h3>
+                    <h3 class="h3 h3-skills">Front-end</h3>
                     <div class="text-left">Javascript, jQuery, Vue.js</div>
                 </li>
                 <li class="box-group-item">
                     <i aria-hidden="true" class="skill-icon fab fa-php"></i>
-                    <h3 class="h3">Server-side</h3>
+                    <h3 class="h3 h3-skills">Server-side</h3>
                     <div class="text-left">PHP, MySQL, Laravel, Wordpress</div>
                 </li>
                 <li class="box-group-item">
                     <i aria-hidden="true" class="skill-icon fas fa-code"></i>
-                    <h3 class="h3">Coding</h3>
-                    <div class="text-left">HTML5, CSS3, Sass</div>
+                    <h3 class="h3 h3-skills">Coding</h3>
+                    <div class="text-left">HTML5, CSS3, Sass, Bootstrap, Github</div>
                 </li>
+
                 <li class="box-group-item">
                     <i aria-hidden="true" class="skill-icon fas fa-palette"></i>
-                    <h3 class="h3">Design</h3>
+                    <h3 class="h3 h3-skills">Design</h3>
                     <div class="text-left">Photoshop, Illustrator, レスポンシブデザイン</div>
                 </li>
                 <li class="box-group-item">
+                    <i aria-hidden="true" class="skill-icon fab fa-github"></i>
+                    <h3 class="h3 h3-skills">Github</h3>
+                    <div class="text-left">Github (command line)</div>
+                </li>
+                <li class="box-group-item">
                     <i aria-hidden="true" class="skill-icon fas fa-server"></i>
-                    <h3 class="h3">Cloud</h3>
+                    <h3 class="h3 h3-skills">Cloud</h3>
                     <div class="text-left">Google Cloud Platform</div>
+                </li>
+
+                <li class="box-group-item">
+                    <i aria-hidden="true" class="skill-icon fas fa-users"></i>
+                    <h3 class="h3 h3-skills">Direction</h3>
+                    <div class="text-left">進行管理, 要件定義, 設計, 技術指導</div>
                 </li>
                 <li class="box-group-item">
                     <i aria-hidden="true" class="skill-icon fas fa-gamepad"></i>
-                    <h3 class="h3">Game Development</h3>
+                    <h3 class="h3 h3-skills">Game Development</h3>
                     <div class="text-left">Unity, C#, UI設計, 企画書・仕様書作成</div>
                 </li>
             </ul>
         </div>
     </section>
 
-<?php
-
-// [id => work]
-$works = [
-    1 => [
-        'title' => '株式会社T様 コーポレートサイト',
-        'date' => '2020.08',
-    ],
-    2 => [
-        'title' => '株式会社O様 コーポレートサイト',
-        'date' => '2020.03',
-    ],
-    3 => [
-        'title' => '株式会社M様 コーポレートサイト(デザイン)',
-        'date' => '2020.02',
-    ],
-    4 => [
-        'title' => '株式会社R様 コーポレートサイト',
-        'date' => '2017.09',
-    ],
-    5 => [
-        'title' => '株式会社J様 コーポレートサイト',
-        'date' => '2017.06',
-    ],
-    6 => [
-        'title' => '株式会社M様 コーポレートサイト',
-        'date' => '2017.04',
-    ],
-]
-?>
-
-    <section id="works" class="section">
+    <section id="works" class="section section-works">
         <div class="container text-center">
             <h2 class="h2">
                 <div>Works</div>
-                <div>制作実績の一部を紹介します。</div>
+                <div class="subtitle">制作実績の一部をご紹介します。</div>
             </h2>
-            
-            <div class="box-group box-group-works">
-                <?php foreach($works as $index => $work) : ?>
-                <a class="box-group-item" href="<?= "/works.php?id={$index}" ?>">
-                    <img src="<?= "./images/works_thumb_{$index}.jpg" ?>" alt="<?= $work['title'] ?>">
+        </div>
 
-                    <div class="box-group-inner">
-                        <h3><?= $work['title'] ?></h3>
-                        <small><?= $work['date'] ?></small>
-                        <span class="button-readmore">Read More</span>
-                    </div>
+        <div class="box-group box-group-works text-center">
+            <?php foreach($works as $index => $work) : ?>
+            <div class="box-group-item">
+                <a class="box-group-inner" href="<?= "/works.php?id={$index}" ?>">
+                    <h3 class="h3 h3-works">
+                        <div><?= $work['title'] ?></div>
+                        <div class="subtitle"><?= $work['date'] ?></div>
+                    </h3>
+                    <div class="btn-readmore">Read More</div>
                 </a>
-                <?php endforeach; ?>
-            </div>        
+                <img class="box-group-image" src="<?= "./images/works_thumb_{$index}.jpg" ?>" alt="<?= $work['title'] ?>">
+            </div>
+            <?php endforeach; ?>
         </div>        
     </section>
+
+    <section id="history" class="section section-history">
+        <div class="container">
+            <h2 class="h2 text-center">
+                <div>History</div>
+                <div class="subtitle">略歴</div>
+            </h2>
+
+            <dl class="dl">
+                <dt class="dt">
+                    <h3 class="h3 h3-history">
+                        <div class="subtitle">2016 〜 2017.09</div>
+                        <div>フリーランスWebデザイナー</div>
+                    </h3>
+                </dt>
+                <dd class="dd">
+                知人のWeb/アプリ制作会社CEOに勧められ、Webデザイナーとして制作請負を開始。並行して Tech Academy Webデザインコースにて学習。<br>
+                2017年から個人事業主として独立し、単価交渉、契約から企画、デザイン、コーディング、運用サポート、コンサルまで個人で担当する。
+                </dd>
+
+                <dt class="dt">
+                    <h3 class="h3 h3-history">
+                        <div class="subtitle">2017.06 〜 2019.12</div>
+                        <div>ゲームクリエイター（プランナー）</div>
+                    </h3>
+                </dt>
+                <dd class="dd">
+                以前から憧れていたゲーム業界に、業務委託社員として参画。<br>
+                ゲームプランナーとして、ソーシャルゲーム新規開発、運用に携わる。<br>
+                企画・仕様書作成、UI設計、仕様策定、ディレクション、Webサイト保守/運用等を担当。
+                </dd>
+
+                <dt class="dt">
+                    <h3 class="h3 h3-history">
+                        <div class="subtitle">2019.12 〜 Now</div>
+                        <div>Web制作チームリーダー</div>
+                    </h3>
+                </dt>
+                <dd class="dd">
+                運用タイトルのサービス終了に伴い、所属会社の本社配属へと転向。<br>
+                Web制作チームリーダーとして、Webデザイン、コーディング、ディレクション、メンバーへの技術指導等、プレイングリーダーとして広く業務に携わる。
+                </dd>
+            </dl>
+        </div>
+    </section>
     
-    <section id="contact" class="section">
+    <section id="contact" class="section section-contact">
         <div class="container text-center">
             <h2 class="h2">
                 <div>Contact</div>
                 <div class="subtitle">お気軽にお問い合わせください。</div>
             </h2>
             <div class="contacts">
-                <a href="mailto:cattyneo@gmail.com"><i aria-label="メール" class="contact-icon fa fa-envelope-o"></i></a>
+                <a class="contact-link" href="mailto:cattyneo@gmail.com"><i aria-label="お問い合わせ" class="contact-icon far fa-envelope"></i></a>
             </div>
         </div>
     </section>
     
-    <footer>
-        <small class="text-center font-alt">&copy; Naohide Takahashi, All Rights Reserved.</small>
+    <footer class="footer">
+        <div class="container">
+            <div class="font-alt text-center">&copy; Naohide Takahashi, All Rights Reserved.</div>
+        </div>
     </footer>
 </body>
 </html>
